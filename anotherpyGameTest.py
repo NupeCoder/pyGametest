cf4d1e008c6b42c87d547df668dfdc6745368aac
Abubakar Liman <limanzaffar@gmail.com>
2022-07-16 23:46:17 +0100

    Add files via upload

--- anotherpyGameTest.py
+++ anotherpyGameTest.py
@@ -4,16 +4,33 @@ pg.init()
 screen = width, height = 400, 400
 white = [255, 255, 255]
 mario = 'C:\\Users\\zaffar\\.vscode\\Pygame\\Mario sprites\\tile007.png' 
-mariorunright = 'C:\\Users\\zaffar\\.vscode\\Pygame\\Mario sprites\\tile009.png'
+right1 = 'C:\\Users\\zaffar\\.vscode\\Pygame\\Mario sprites\\tile008.png'
+right2 = 'C:\\Users\\zaffar\\.vscode\\Pygame\\Mario sprites\\tile009.png'
+rightjump = 'C:\\Users\\zaffar\\.vscode\\Pygame\\Mario sprites\\tile010.png'
+left1 = 'C:\\Users\\zaffar\\.vscode\\Pygame\\Mario sprites\\tile004.png'
+left2 = 'C:\\Users\\zaffar\\.vscode\\Pygame\\Mario sprites\\tile003.png'
+left3 = 'C:\\Users\\zaffar\\.vscode\\Pygame\\Mario sprites\\tile002.png'
+leftjump = 'C:\\Users\\zaffar\\.vscode\\Pygame\\Mario sprites\\tile001.png'
 
 class Mario(object):
     def __init__(self):
-        self.image = pg.image.load(mario)
         self.position = [200, 200]
-    
+        self.current = 0
+        self.right = []
+        self.left = []
+        self.right.append(pg.image.load(mario))
+        self.right.append(pg.image.load(right1))
+        self.right.append(pg.image.load(right2))
+        self.left.append(pg.image.load(left1))
+        self.left.append(pg.image.load(left2))
+        self.left.append(pg.image.load(left3))
+        self.image = pg.image.load(mario)
+
+
     def move(self, x, y):
         self.position[0] += x
         self.position[1] += y
+        
 
     def draw(self, surface):
         surface.blit(self.image, self.position)
@@ -28,7 +45,7 @@ class Run(object):
     def run(self):
         run = True
         while run:
-            self.clock.tick(60)
+            self.clock.tick(10)
             keys = pg.key.get_pressed()
             for event in pg.event.get():
                 if event.type == pg.QUIT:
@@ -37,17 +54,36 @@ class Run(object):
                 
                 if event.type == pg.KEYDOWN:
                     if event.key == pg.K_UP:
-                        self.player.position[1] -= 10
+                        self.player.move(0, -50)
                         
             if keys[pg.K_RIGHT]:
-                self.player.position[0] += 10
-                self.player.image = pg.image.load(mariorunright)
+                self.player.move(10, 0)
+                self.player.image = self.player.right[self.player.current]
+                for i in range(len(self.player.right)-1):
+                    self.player.current += 1
+                    print(self.player.current)
+                    
+                if self.player.current >= (len(self.player.right)):
+                    self.player.current = 0
+                    
+
             if keys[pg.K_LEFT]:
-                self.player.position[0] -= 10
+                self.player.move(-10, 0)
+                self.player.image = self.player.left[self.player.current]
+                for i in range(len(self.player.left)-1):
+                    self.player.current += 1
+                    print(self.player.current)
+                    
+                if self.player.current >= (len(self.player.left)):
+                    self.player.current = 0
 
             if self.player.position[1] < 200:
-                pg.time.wait(500)
-                self.player.position[1] += 5
+                pg.time.wait(50)
+                if self.player.image in self.player.right:
+                    self.player.image = pg.image.load(rightjump)
+                if self.player.image in self.player.left:
+                    self.player.image = pg.image.load(leftjump)
+                self.player.position[1] += 10
 
             self.screen.fill(white)
             self.player.draw(self.screen)
